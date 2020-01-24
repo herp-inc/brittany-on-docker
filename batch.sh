23c8de219f60931a20797b16cb5e19522869cb9f
coord.e <me@coord-e.com>
2020-01-24 02:17:14 +0000

    Build and push latest image in batch.sh

--- batch.sh
+++ batch.sh
@@ -13,10 +13,8 @@ function check_image_exists() {
 }
 
 function try_version() {
-  local image_repo=$1
+  local image_name=$1
   local version=$2
-
-  local image_name=${image_repo}:${version}
 
   if $SKIP_BUILT; then
     if check_image_exists ${image_name}; then
@@ -41,16 +39,19 @@ function main() {
   local image_repo=$1
   if [ $# -eq 1 ]; then
     local versions=$(cabal list --simple-output brittany | sed -n 's/brittany \(.*\)/\1/p' | tr '\n' ' ')
+    # TODO: Find a canonical way to obtain the latest version from Hackage.
+    local latest=$(echo "$versions" | awk '{print $NF}')
   else
+    local latest=$2
     shift
     local versions="$@"
   fi
 
-  echo "Building for versions: $versions"
+  echo "Building for versions: $versions (latest: $latest)"
 
   local completed=""
   for v in $versions; do
-    try_version $image_repo $v || continue
+    try_version $image_repo:$v $v || continue
     completed="$completed $v"
   done
 
@@ -58,6 +59,11 @@ function main() {
     echo "Some versions could not be built."
     echo "Tried: $versions"
     echo "Completed: $completed"
+    exit 1
+  fi
+
+  if ! try_version $image_repo:latest $latest; then
+    echo "Could not build the latest version: $latest"
     exit 1
   fi
 }
